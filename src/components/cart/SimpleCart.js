c5ec98618ee3d50ee76b6ccf530295bf5bd65edb
William-Moreno <intrcptr@comcast.net>
2021-04-06 22:49:53 -0700

    cleaned up extraneous imports

--- src/components/cart/SimpleCart.js
+++ src/components/cart/SimpleCart.js
@@ -2,42 +2,18 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { removeFromCart } from '../../store/cart.js';
 import { If } from '../if/If.js';
-import { makeStyles } from '@material-ui/core/styles';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
-import ListItemAvatar from '@material-ui/core/ListItemAvatar';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
 import ListItemText from '@material-ui/core/ListItemText';
-import Avatar from '@material-ui/core/Avatar';
 import IconButton from '@material-ui/core/IconButton';
-import FormGroup from '@material-ui/core/FormGroup';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import Grid from '@material-ui/core/Grid';
-import Typography from '@material-ui/core/Typography';
-import FolderIcon from '@material-ui/icons/Folder';
+// import Typography from '@material-ui/core/Typography';
 import DeleteIcon from '@material-ui/icons/Delete';
 import Paper from '@material-ui/core/Paper';
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-    maxWidth: 752,
-  },
-  demo: {
-    backgroundColor: theme.palette.background.paper,
-  },
-  title: {
-    margin: theme.spacing(4, 0, 2),
-  },
-}));
-
 
 const CartDisplay = (props) => {
-  const classes = useStyles();
-  const [dense, setDense] = React.useState(false);
-  const [secondary, setSecondary] = React.useState(false);
+  const [dense] = React.useState(false);
   return (
 
 <div>
--- src/components/storefront/Categories.js
+++ src/components/storefront/Categories.js
@@ -3,17 +3,9 @@ import { connect } from 'react-redux';
 import { selectCategory } from '../../store/categories.js';
 import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
 
-const useStyles = makeStyles({
-  uppercase: {
-    textTransform: 'uppercase',
-  },
-});
 
 const CategoryPicker = (props) => {
-  const classes = useStyles();
   return (
     <div>
       <div>
